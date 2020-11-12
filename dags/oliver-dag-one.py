10728172cbed1b2f318c1d4b92b63a3a3eb65a17
Oliver Uhrig <oliver_uhrig@manulife.com>
2020-11-12 08:02:34 -0500

    updated dag name

--- dags/oliver-dag-one.py
+++ dags/oliver-dag-one.py
@@ -13,7 +13,7 @@ now_to_the_hour = (
     now - timedelta(0, 0, 0, 0, 0, 3)
 ).replace(minute=0, second=0, microsecond=0)
 START_DATE = now_to_the_hour
-DAG_NAME = 'test_dag_v1'
+DAG_NAME = 'oliver-dag-one'
 
 default_args = {
     'owner': 'airflow',
